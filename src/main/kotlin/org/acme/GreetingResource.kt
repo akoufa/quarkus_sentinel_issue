9591ea96fb3a5c7a6cc38187589e61c0a9ee43da
Alex Koufatzis <akoufa@hotmail.com>
2021-02-10 12:24:05 +0100

    Update docker-compose.yml file. Sentinel issue persists

--- src/main/kotlin/org/acme/GreetingResource.kt
+++ src/main/kotlin/org/acme/GreetingResource.kt
@@ -10,10 +10,10 @@ import javax.ws.rs.core.MediaType
 
 @ApplicationScoped
 @Path("/hello")
-class GreetingResource(val redisClient: RedisClient, @RedisClientName("standalone") val standaloneClient: RedisClient) {
+class GreetingResource(val redisClient: RedisClient) {
 
     init {
-        standaloneClient.set(listOf("some_key", "it works"))
+        redisClient.set(listOf("some_key", "it works"))
     }
 
     @GET
@@ -21,14 +21,6 @@ class GreetingResource(val redisClient: RedisClient, @RedisClientName("standalon
     @Produces(MediaType.TEXT_PLAIN)
     fun hello(): String {
         val result = redisClient.get("some_key")?.toString() ?: return "value not found"
-        return result
-    }
-
-    @GET
-    @Path("/standalone")
-    @Produces(MediaType.TEXT_PLAIN)
-    fun standaloneHello(): String {
-        val result = standaloneClient.get("some_key")?.toString() ?: return "value not found"
         return result
     }
 }
